23e58dc7da69bfda824a5081d5118fbbaf48e75f
Marcelo Benjamin <marcelo.dfx@gmail.com>
2019-05-17 17:12:34 -0300

    Autenticacao pronta. Falta cadastro e reset de senha.

--- app/src/main/java/iesb/mobile/pdm5sem/MainActivity.java
+++ app/src/main/java/iesb/mobile/pdm5sem/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -23,45 +24,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String email = findViewById(R.id.usuarioTxt).toString();
-        String senha = findViewById(R.id.senhaTxt).toString();
-        logar(email,senha);
-
-        mAuth.createUserWithEmailAndPassword("marcelo.dfx@gmail.com", "marcelo")
+        mAuth.createUserWithEmailAndPassword("marcelo.dfx@gmail.com", "123456")
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             FirebaseUser user = mAuth.getCurrentUser();
-                            Toast.makeText(MainActivity.this, "Usuário criado", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), "Usuário criado", Toast.LENGTH_SHORT).show();
                         } else {
-                            Toast.makeText(MainActivity.this, "Usuário já criado!", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), "Usuário já criado!", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
 
+
+
     }
 
-    public void logar(final String email, final String senha) {
-        Button btac = findViewById(R.id.btacessar);
-        btac.setOnClickListener(new View.OnClickListener() {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        singIn();
+    }
+
+    public void singIn() {
+        final EditText edEmail = findViewById(R.id.emailIN);
+        final EditText edSenha = findViewById(R.id.senhaIN);
+        final Button singIN = findViewById(R.id.singIN);
+
+        singIN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mAuth.signInWithEmailAndPassword(email, senha)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(MainActivity.this, "Usuário Logado", Toast.LENGTH_SHORT).show();
-                                    Intent go = new Intent("homeAcao");
-                                    startActivity(go);
-
-                                } else {
-                                    Toast.makeText(MainActivity.this, "Falha de acesso", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
+                if((edEmail.getText().toString()).equals("") || (edSenha.getText().toString()).equals("")) {
+                    Toast.makeText(getApplicationContext(),"Obs: Os campos não podem ser nulos", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    singIN.setVisibility(View.INVISIBLE);
+                    logar(edEmail.getText().toString(), edSenha.getText().toString());
+                }
             }
         });
+    }
+
+    private void logar(String email, String senha) {
+        final Button singIN = findViewById(R.id.singIN);
+        mAuth.signInWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent go = new Intent("homeAcao");
+                            startActivity(go);
+                        } else {
+                            singIN.setVisibility(View.VISIBLE);
+                            Toast.makeText(getApplicationContext(), "Usuário ou senha incorreto!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
